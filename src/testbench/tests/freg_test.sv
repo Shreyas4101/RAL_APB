ed459204ccd9ad913c3f9c780887676587c8cf76
Shreyas <122604462+Shreyas4101@users.noreply.github.com>
2025-08-05 15:28:43 +0530

    Update freg_test.sv

--- src/testbench/tests/freg_test.sv
+++ src/testbench/tests/freg_test.sv
@@ -29,20 +29,18 @@ class freg_test extends uvm_test;
 
   `uvm_component_utils(freg_test)
 
-  fr1_seq seq1; 
-  fr2_seq seq2;
-  fr3_seq seq3;
-  fr4_seq seq4;
+  rst1_seq seq1;
+  rst2_seq seq2;
+  rst3_seq seq3;
+  rst4_seq seq4;
+  fr1_seq seq5; 
+  fr2_seq seq6;
+  fr3_seq seq7;
+  fr4_seq seq8;
 
   ral_env env;
 
-
-  /* apb_write_read_s1 write_read_s1_pkt;
-  apb_write_read_s2 write_read_s2_pkt;
-
-  apb_write_2_read_s1 write_2_read_s1_pkt;
-  apb_write_2_read_s2 write_2_read_s2_pkt;
-
+  /*
   apb_slave_toggle slave_toggle_pkt;
 
   apb_transfer_invalid transfer_invalid_pkt;*/
@@ -56,14 +54,17 @@ class freg_test extends uvm_test;
     super.build_phase(phase);
     env = ral_env::type_id::create("env",this);
 
-    seq1 = fr1_seq::type_id::create("seq1", this);
-    seq2 = fr2_seq::type_id::create("seq2", this);
-    seq3 = fr3_seq::type_id::create("seq3", this);
-    seq4 = fr4_seq::type_id::create("seq4", this);
-    /*  write_read_s1_pkt    = apb_write_read_s1::type_id::create("write_read_s1_pkt", this);
-    write_read_s2_pkt    = apb_write_read_s2::type_id::create("write_read_s2_pkt", this);
-    write_2_read_s1_pkt  = apb_write_2_read_s1::type_id::create("write_2_read_s2_pkt", this);
-    write_2_read_s2_pkt  = apb_write_2_read_s2::type_id::create("write_2_read_s1_pkt", this);
+
+    seq1 = rst1_seq::type_id::create("seq5", this);
+    seq2 = rst2_seq::type_id::create("seq6", this);
+    seq3 = rst3_seq::type_id::create("seq8", this);
+    seq4 = rst4_seq::type_id::create("seq7", this);
+    seq5 = fr1_seq::type_id::create("seq1", this);
+    seq6 = fr2_seq::type_id::create("seq2", this);
+    seq7 = fr3_seq::type_id::create("seq3", this);
+    seq8 = fr4_seq::type_id::create("seq4", this);
+    
+    /*
     slave_toggle_pkt     = apb_slave_toggle::type_id::create("slave_toggle_pkt", this);
     transfer_invalid_pkt = apb_transfer_invalid::type_id::create("transfer_invalid_pkt", this);*/
   endfunction
@@ -99,6 +100,37 @@ class freg_test extends uvm_test;
     phase.drop_objection(this);
     phase.phase_done.set_drain_time(this, 20);
   endtask
+  
+    virtual task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    seq5.regmodel = env.regmodel;
+    seq5.start(env.agent.seqr);
+    phase.drop_objection(this);
+    phase.phase_done.set_drain_time(this, 20);
+  endtask
 
+  virtual task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    seq6.regmodel = env.regmodel;
+    seq6.start(env.agent.seqr);
+    phase.drop_objection(this);
+    phase.phase_done.set_drain_time(this, 20);
+  endtask
+
+  virtual task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    seq7.regmodel = env.regmodel;
+    seq7.start(env.agent.seqr);
+    phase.drop_objection(this);
+    phase.phase_done.set_drain_time(this, 20);
+  endtask
+
+  virtual task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    seq8.regmodel = env.regmodel;
+    seq8.start(env.agent.seqr);
+    phase.drop_objection(this);
+    phase.phase_done.set_drain_time(this, 20);
+  endtask
 
 endclass
