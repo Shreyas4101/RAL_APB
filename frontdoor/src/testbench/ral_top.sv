0d4a7a697f22d492619144c9e0f8d087a0ce087f
Shreyas4101 <122604462+Shreyas4101@users.noreply.github.com>
2025-08-06 16:46:49 +0530

    Updated

--- frontdoor/src/testbench/ral_top.sv
+++ frontdoor/src/testbench/ral_top.sv
@@ -21,10 +21,10 @@ module tb;
   always #10 pclk = ~pclk;
   
   initial begin
-  // presetn=0;
-  // #10 presetn=1;
-//    presetn=0;
-//   #10 presetn = 1;
+   presetn=0;
+   #10 presetn=1;
+    presetn=0;
+   #10 presetn = 1;
   end
   initial begin
   uvm_config_db#(virtual ral_interface.mp_drv)::set(null, "*", "vif", vif.mp_drv);
